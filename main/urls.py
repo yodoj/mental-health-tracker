abf0f2f016b1c8d988ec55c609f06821849d021c
yodoj <nadiraalia4@gmail.com>
2024-09-11 14:21:56 +0700

    Menautkan link pws

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 from django.urls import path
 from main.views import show_main
 
 app_name = 'main'
 
 urlpatterns = [
+    path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
+    path('xml/', show_xml, name='show_xml'),
     path('', show_main, name='show_main'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,44 @@
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
+from main.forms import MoodEntryForm
+from main.models import MoodEntry
 from django.shortcuts import render
 
 # Create your views here.
 def show_main(request):
+    mood_entries = MoodEntry.objects.all()
     context = {
-        'npm' : '2306165692',
         'name': 'Nadira Aliya Nashwa',
-        'class': 'PBP C'
+        'class': 'PBP C',
+        'npm' : '2306165692',
+        'mood_entries': mood_entries
     }
 
     return render(request, "main.html", context)
+
+def create_mood_entry(request):
+    form = MoodEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_mood_entry.html", context)
+
+def show_xml(request):
+    data = MoodEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MoodEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
