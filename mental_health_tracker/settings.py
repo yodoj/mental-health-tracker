149cec8c341cd1ad5344ecfb815dfe70e36f9bb0
yodoj <nadiraalia4@gmail.com>
2024-09-11 14:33:58 +0700

    Menautkan link pws

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://nadira-aliya-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://nadira-aliya-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://nadira-aliya-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://nadira-aliya-mentalhealthtracker.pbp.cs.ui.ac.id/"] # Link
