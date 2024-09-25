80773ab3eb4fc02b0ced5379a7c5d036da414cc2
yodoj <nadiraalia4@gmail.com>
2024-09-25 13:28:20 +0700

    Menyelesaikan tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from main.views import show_main
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
+from main.views import edit_mood
+from main.views import delete_mood
 
 app_name = 'main'
 
@@ -17,4 +19,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
+    path('delete/<uuid:id>', delete_mood, name='delete_mood'),
 ]
